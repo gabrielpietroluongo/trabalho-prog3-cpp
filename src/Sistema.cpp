d517a8e5e1779e81faffd2299a5b2518c2a64063
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2018-06-19 01:38:00 -0300

    Implementação parcial da classe "Sistema"

--- src/Sistema.cpp
+++ src/Sistema.cpp
@@ -6,4 +6,47 @@
 
 namespace prog3 {
 
+    Sistema::Sistema() {
+        //Void?
+    }
+
+    Sistema::~Sistema() {
+        //TODO
+    }
+
+    void Sistema::WIP_carrega_dados() {
+
+    }
+
+    void Sistema::adicionaDocente(int codigo, string nome, string departamento) {
+        Docente *d = new Docente(codigo, nome, departamento);
+        this->docentes.insert(pair<int, Docente>(codigo, d));
+    }
+
+    void Sistema::adicionaDiscente(long matricula, string nome, int codigoCurso) {
+        //TODO
+    }
+
+    void Sistema::adicionaProducao(int codigo, string titulo, bool qualificada) {
+        //TODO
+    }
+
+    void Sistema::adicionaCurso(int codigo, string nome, bool grad) {
+        Curso* c = new Curso(nome, codigo, grad);
+        this->cursos.insert(pair<int, Curso>(codigo, nome));
+    }
+
+    void Sistema::adicionaDisciplina(int codigoMateria, string nome, int codigoDocente, int cargaSemanal,
+                            int cargaSemestral, int codigoCurso) {
+        //TODO
+    }
+
+    void Sistema::adicionaOrientacaoGrad(int codigoDocente, long matriculaDiscente, int codigoCurso, int cargaSemanal) {
+        //TODO
+    }
+
+    void Sistema::adicionaOrientacaoPos(int codigoDocente, int matriculaDiscente, int dataIngresso,
+                               string programa, int cargaSemanal) {
+        //TODO
+    }
 }
--- src/Sistema.h
+++ src/Sistema.h
@@ -5,17 +5,40 @@
 #ifndef TRABALHO_PROG3_SISTEMA_H
 #define TRABALHO_PROG3_SISTEMA_H
 
+#include <string>
+#include <iostream>
+#include <map>
+#include <vector>
+
 //Includes do sistema
 #include "Docente.h"
 #include "Curso.h"
 #include "Producao.h"
 #include "Discente.h"
 #include "Atividade.h"
-#include "Disciplina.h"
 
 namespace prog3 {
 
     class Sistema {
+    private:
+        map<int, Docente*> docentes;
+        vector<Producao*> producoes;
+        map<int, Curso*> cursos;
+        map<long, Discente*> discentes;
+        vector<Atividade*> atividades;
+    public:
+        Sistema();
+        ~Sistema();
+        void WIP_carrega_dados();
+        void adicionaDocente(int codigo, string nome, string departamento);
+        void adicionaDiscente(long matricula, string nome, int codigoCurso);
+        void adicionaProducao(int codigo, string titulo, bool qualificada);
+        void adicionaCurso(int codigo, string nome, bool grad);
+        void adicionaDisciplina(int codigoMateria, string nome, int codigoDocente, int cargaSemanal,
+                                int cargaSemestral, int codigoCurso);
+        void adicionaOrientacaoGrad(int codigoDocente, long matriculaDiscente, int codigoCurso, int cargaSemanal);
+        void adicionaOrientacaoPos(int codigoDocente, int matriculaDiscente, int dataIngresso,
+                                   string programa, int cargaSemanal);
 
     };
 
