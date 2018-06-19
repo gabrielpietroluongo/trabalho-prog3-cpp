837172fb0244dd00afecc69886c1c5c1bc1f3f0a
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2018-06-18 21:15:31 -0300

    Implementação do namespace "prog3"

--- src/Docente.h
+++ src/Docente.h
@@ -6,19 +6,26 @@
 
 using namespace std;
 
-class Docente {
-private:
-    int codigo;
-    string nome;
-    string departamento;
-public:
-    Docente(int = 0, string = "", string = "");
-    ~Docente();
-    void print();
-    string to_string();
-    friend ostream& operator<<(ostream& os, const Docente& doc);
-    friend ostream& operator<<(ostream& os, const Docente* doc);
-};
+namespace prog3 {
 
+    class Docente {
+    private:
+        int codigo;
+        string nome;
+        string departamento;
+    public:
+        Docente(int = 0, string = "", string = "");
 
+        ~Docente();
+
+        void print();
+
+        string to_string();
+
+        friend ostream &operator<<(ostream &os, const Docente &doc);
+
+        friend ostream &operator<<(ostream &os, const Docente *doc);
+    };
+
+}
 #endif //TRABALHO_PROG3_DOCENTE_H
